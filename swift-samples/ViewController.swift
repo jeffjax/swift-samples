bffb4a908f524a859b3039ee6006a3ef21f59da0
Jeff Jackson <jjackson@esri.com>
2014-07-22 14:46:02 -0400

    Initial commit

---
+++ swift-samples/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift-samples
+//
+//  Created by Jeff Jackson on 7/22/14.
+//  Copyright (c) 2014 Esri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
